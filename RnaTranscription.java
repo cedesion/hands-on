aafdb5352fe821c4a4679205e6d27393db3931dd
Ritik Raj <95036375+cedesion@users.noreply.github.com>
2023-06-30 14:23:26 +0530

    Create RnaTranscription.java

---
+++ RnaTranscription.java
@@ -0,0 +1,10 @@
+class RnaTranscription {
+    String transcribe(String dnaStrand) {
+        return dnaStrand
+            .replace('A', 'U')
+            .replace('T', 'A')
+            .replace('G', 'X') 
+            .replace('C', 'G')
+            .replace('X', 'C');
+    }
+}
